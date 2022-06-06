bc01be3c9ab01b85d0653118d861118c23f1961b
EC2 Default User <ec2-user@ip-172-31-29-130.us-west-2.compute.internal>
2022-06-06 13:47:20 +0000

    test

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=shivaprasadkamath/udacityproject4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login 
+docker image tag nanodegree4 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
